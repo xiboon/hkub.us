588bde5b1cd2bca7b70bd77d4d893047ac9520a6
Xiboon <xiboon@femboy.com>
2021-06-29 22:27:41 +0200

    useless syf

--- js/index.js
+++ js/index.js
@@ -12,22 +12,19 @@ setInterval(() => {
   const age = (now - date) / 3600 / 24 / 365;
   document.getElementById('age').innerHTML = roundUp(age, 7);
 }, 2000);
-
-// themes
-
 function changeTheme() {
   theme = !theme;
   if (!theme) {
     document.getElementById('main').className = 'main-light';
     document.getElementById('description').className = 'text-light';
     document.getElementById('nick').className = 'text-light';
-    document.getElementById('btn').className = 'button-light';
-    document.getElementById('btn').innerHTML = 'dark_mode';
+    document.getElementById('btn').className = 'button-light'.innerHTML =
+      'dark_mode';
   } else {
     document.getElementById('main').className = 'main';
     document.getElementById('description').className = 'text';
     document.getElementById('nick').className = 'text';
-    document.getElementById('btn').className = 'button';
-    document.getElementById('btn').innerHTML = 'light_mode';
+    document.getElementById('btn').className = 'button'.innerHTML =
+      'light_mode';
   }
 }
