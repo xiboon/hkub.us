1ec583f35ab07ca71f91cd0718e94efa6488b899
Xiboon <xiboon@femboy.com>
2021-06-30 03:29:50 +0200

    sex

--- js/theme.js
+++ js/theme.js
@@ -1,17 +1,18 @@
 let theme = true;
 function changeTheme() {
   theme = !theme;
+  let btn = document.getElementsByClassName('btn');
   if (!theme) {
     document.getElementById('main').className = 'main-light';
     document.getElementById('description').className = 'text-light';
     document.getElementById('nick').className = 'text-light';
-    document.getElementById('btn').className = 'button-light';
-    document.getElementById('btn').innerHTML = 'dark_mode';
+    btn.className = 'button-light';
+    btn.innerHTML = 'dark_mode';
   } else {
     document.getElementById('main').className = 'main';
     document.getElementById('description').className = 'text';
     document.getElementById('nick').className = 'text';
-    document.getElementById('btn').className = 'button';
-    document.getElementById('btn').innerHTML = 'light_mode';
+    btn.className = 'button';
+    btn.innerHTML = 'light_mode';
   }
 }
