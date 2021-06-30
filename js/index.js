d5a0a3bb3971e4c2eabb253b0fc22f36060be250
Xiboon <xiboon@femboy.comtf>
2021-06-30 16:27:18 +0200

    mniej js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,8 @@
 const date = 1259098620
 let last
 
-function roundUp(num, precision) {
-  precision = Math.pow(10, precision)
-  return Math.ceil(num * precision) / precision
-}
-
 setInterval(() => {
   const now = Math.round(Date.now() / 1000)
   const age = (now - date) / 3600 / 24 / 365
-  document.getElementById('age').innerHTML = roundUp(age, 7)
+  document.getElementById('age').innerHTML = age.toFixed(7)
 }, 2000)
