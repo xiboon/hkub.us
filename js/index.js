b10156faf690acd75deee4b91ec7c6a06ff56dfa
Xiboon <xiboon@femboy.com>
2021-06-30 03:18:55 +0200

    duze zmiany w huj

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 const date = 1259098620;
-let theme = true;
 let last;
 
 function roundUp(num, precision) {
@@ -12,19 +11,3 @@ setInterval(() => {
   const age = (now - date) / 3600 / 24 / 365;
   document.getElementById('age').innerHTML = roundUp(age, 7);
 }, 2000);
-function changeTheme() {
-  theme = !theme;
-  if (!theme) {
-    document.getElementById('main').className = 'main-light';
-    document.getElementById('description').className = 'text-light';
-    document.getElementById('nick').className = 'text-light';
-    document.getElementById('btn').className = 'button-light'.innerHTML =
-      'dark_mode';
-  } else {
-    document.getElementById('main').className = 'main';
-    document.getElementById('description').className = 'text';
-    document.getElementById('nick').className = 'text';
-    document.getElementById('btn').className = 'button'.innerHTML =
-      'light_mode';
-  }
-}
