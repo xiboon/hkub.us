4e1a255bad91596758a4b1e5116f4854fbfabcd7
Xiboon <xiboon@femboy.com>
2021-06-30 03:36:24 +0200

    sex

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-const date = 1259098620;
-let last;
+const date = 1259098620
+let last
 
 function roundUp(num, precision) {
-  precision = Math.pow(10, precision);
-  return Math.ceil(num * precision) / precision;
+  precision = Math.pow(10, precision)
+  return Math.ceil(num * precision) / precision
 }
 
 setInterval(() => {
-  const now = Math.round(Date.now() / 1000);
-  const age = (now - date) / 3600 / 24 / 365;
-  document.getElementById('age').innerHTML = roundUp(age, 7);
-}, 2000);
+  const now = Math.round(Date.now() / 1000)
+  const age = (now - date) / 3600 / 24 / 365
+  document.getElementById('age').innerHTML = roundUp(age, 7)
+}, 2000)
--- js/theme.js
+++ js/theme.js
@@ -1,21 +1,21 @@
-let theme = true;
+let theme = true
 function changeTheme() {
-  theme = !theme;
-  let btn = document.getElementById('btn');
-  let main = document.getElementById('main');
-  let dscp = document.getElementById('description');
-  let nick = document.getElementById('nick');
+  theme = !theme
+  let btn = document.getElementById('btn')
+  let main = document.getElementById('main')
+  let dscp = document.getElementById('description')
+  let nick = document.getElementById('nick')
   if (!theme) {
-    main.className = 'main-light';
-    dscp.className = 'text-light';
-    nick.className = 'text-light';
-    btn.className = 'button-light';
-    btn.innerHTML = 'dark_mode';
+    main.className = 'main-light'
+    dscp.className = 'text-light'
+    nick.className = 'text-light'
+    btn.className = 'button-light'
+    btn.innerHTML = 'dark_mode'
   } else {
-    main.className = 'main';
-    dscp.className = 'text';
-    nick.className = 'text';
-    btn.className = 'button';
-    btn.innerHTML = 'light_mode';
+    main.className = 'main'
+    dscp.className = 'text'
+    nick.className = 'text'
+    btn.className = 'button'
+    btn.innerHTML = 'light_mode'
   }
 }
