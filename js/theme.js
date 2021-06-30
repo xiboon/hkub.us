b8927648f338463b6b2a8db025281fb360012dee
Xiboon <xiboon@femboy.comtf>
2021-06-30 18:16:24 +0200

    color scheme change

--- js/theme.js
+++ js/theme.js
@@ -2,8 +2,11 @@ function theme() {
   body = document.querySelector('body')
   dot = document.querySelector('.theme-dot')
   navbar = document.querySelector('.navbar')
-
+  icons = document.getElementsByClassName('navbaricon')
+  ;[...icons].forEach(function (div) {
+    div.classList.toggle('darkicon')
+  })
   body.classList.toggle('dark')
-  navbar.classList.toggle('dark')
+  navbar.classList.toggle('navdark')
   dot.classList.toggle('dark')
 }
