d50cc7caecbe5e604d7283b3f0dc0ecdc80505a0
Xiboon <xiboon@femboy.comtf>
2021-07-01 17:16:00 +0200

    tak

--- js/index.js
+++ js/index.js
@@ -1,17 +1,21 @@
 const date = 1259098620
 let last
-let themea = true
 
 setInterval(() => {
   const now = Math.round(Date.now() / 1000)
   const age = (now - date) / 3600 / 24 / 365
   document.getElementById('age').innerHTML = age.toFixed(7)
 }, 2000)
-if (localStorage.theme) theme()
+function parseBool(val) {
+  return val === 'true'
+}
+
+let themea = parseBool(localStorage.theme) ?? true
+
+if (!themea) theme()
 function clicked() {
   let check = easteregg()
   if (check) return
-  themea = !themea
+  localStorage.setItem('theme', !parseBool(localStorage.theme))
   theme()
-  localStorage.theme = themea
 }
