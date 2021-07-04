3f77579a9f9c8dd4434d59b5bef44b9f3b0b5208
Xiboon <xiboon@femboy.comtf>
2021-07-04 02:15:42 +0200

    uhhhhhhhh

--- js/theme.js
+++ js/theme.js
@@ -2,11 +2,13 @@ function theme() {
   body = document.querySelector('body')
   dot = document.querySelector('.theme-dot')
   navbar = document.querySelector('.navbar')
+  gh = document.querySelector('.gh')
   icons = document.getElementsByClassName('navbaricon')
   ;[...icons].forEach(function (div) {
     div.classList.toggle('darkicon')
   })
   body.classList.toggle('dark')
   navbar.classList.toggle('navdark')
+  gh.classList.toggle('darktext')
   dot.classList.toggle('dark')
 }
