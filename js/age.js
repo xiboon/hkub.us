135dfe5c23bfbcbbb0b9ad12fe89b3006e32e0d4
xiboon <sexiarz@sexiarz.pl>
2021-11-30 20:50:03 +0100

    redesign

--- js/age.js
+++ js/age.js
@@ -1,6 +1,6 @@
 const date = 1259098620;
 const now = Math.round(Date.now() / 1000);
 const age = (now - date) / 3600 / 24 / 365;
-document.addEventListener("DOMContentLoaded", () => {
-document.getElementById("age").innerHTML = age.toFixed(3);
-})
+document.addEventListener('DOMContentLoaded', () => {
+	document.querySelector("strong").innerHTML = age.toFixed(3);
+});
