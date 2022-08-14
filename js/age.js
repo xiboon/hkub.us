44fb2c39c29b80b08e57955a41a0e86fcd6dc008
xiboon <38988733+xiboon@users.noreply.github.com>
2022-08-14 18:28:46 +0200

    kubus dpeloy

--- js/age.js
+++
@@ -1,6 +0,0 @@
-const date = 1259098620;
-const now = Math.round(Date.now() / 1000);
-const age = (now - date) / 3600 / 24 / 365;
-document.addEventListener('DOMContentLoaded', () => {
-	document.querySelector("strong").innerHTML = age.toFixed(3);
-});
